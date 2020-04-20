6ef04bf94b4960a673545444b16953ba0313d459
Yutai <yutaizh@gmail.com>
2020-04-20 14:01:22 -0400

    grep aliases, added to install yaml

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -2,6 +2,8 @@
 # Use colors in coreutils utilities output
 alias ls='ls --color=auto'
 alias grep='grep --color'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
 
 # ls aliases
 alias ll='ls -lah'
