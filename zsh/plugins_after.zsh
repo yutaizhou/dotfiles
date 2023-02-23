62244700cf0cc029ccc61e60453ca1b411f8625d
Yutai Zhou <yutaizh@gmail.com>
2023-02-23 10:06:19 -0500

    fixed fzf vim zsh incompat

--- zsh/plugins_after.zsh
+++ zsh/plugins_after.zsh
@@ -1,6 +1,11 @@
 eval "$(starship init zsh)"
 eval "$(zoxide init zsh)"
+
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
 source ~/.dotfiles/zsh/plugins/zsh-vi-mode/zsh-vi-mode.plugin.zsh
-# must be at end of zshrc. allowed before conda init block?
+function zvm_after_init() {
+  [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+}
+
 source ~/.dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
