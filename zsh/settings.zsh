96f84e73d96ba3533f600170c83442ab6306757f
Yutai Zhou <yutaizh@gmail.com>
2023-03-01 13:00:26 -0500

    zsh completion

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -6,5 +6,19 @@ setopt incappendhistory
 setopt extendedhistory
 
 setopt interactivecomments
-
 export EDITOR=vim
+
+#Initialize completion
+autoload -Uz compinit && compinit -i
+zstyle ':completion:*' menu select=4
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' list-colors "${(@s.:.)LS_COLORS}"
+zmodload zsh/complist
+# Use vim style navigation keys in menu completion
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+
+# Initialize editing command line
+autoload -U edit-command-line && zle -N edit-command-line
