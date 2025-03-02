1ab3ebcab58ba8bd4344075a9ed56b26c5255b90
Yutai Zhou <yutaizh@gmail.com>
2025-03-02 12:57:55 -0800

    fzf

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,8 +1,6 @@
+source <(fzf --zsh)
 eval "$(starship init zsh)"
 eval "$(zoxide init zsh)"
-source <(fzf --zsh)
 eval $(thefuck --alias fk)
-
-[ -f $HOME/.fzf.zsh ] && source $HOME/.fzf.zsh
 
 source $(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
