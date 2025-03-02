166eec0e40e32929839befd39c8c1833846b587b
Yutai Zhou <yutaizh@gmail.com>
2025-03-02 17:58:32 +0000

    more more

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,6 +1,7 @@
 [ -d $HOME/bin ] && path_prepend "$HOME/bin"
 [ -d $HOME/.local/bin ] && path_prepend "$HOME/.local/bin"
 [ -d $HOME/.dotfiles/bin ] && path_prepend "$HOME/.dotfiles/bin"
+[ -d $HOME/homebrew/bin/ ] && path_prepend "$HOME/homebrew/bin/"
 [ -d /home/linuxbrew/.linuxbrew/bin ] && path_prepend /home/linuxbrew/.linuxbrew/bin
 [ -d /opt/homebrew/bin ] && path_prepend /opt/homebrew/bin
 [ -d /opt/homebrew/sbin ] && path_prepend /opt/homebrew/sbin
