ab5e02879c7d5e4347ad35a51fe0a29e68445c00
Yutai Zhou <yutaizh@gmail.com>
2025-03-01 22:05:56 -0800

    moved around

--- bash/plugins.bash
+++ bash/plugins.bash
@@ -1,3 +1,11 @@
+
+# mac stuff >> 
+export BASH_SILENCE_DEPRECATION_WARNING=1
+eval "$(/opt/homebrew/bin/brew shellenv)"
+export TERM=screen-256color-bce
+# <<
+
+
 eval "$(starship init bash)"
 eval "$(fzf --bash)"
 eval "$(zoxide init bash)"
