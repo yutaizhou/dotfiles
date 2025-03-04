44a84bd59c3ffb3c64579ba1a8fc869de858d2ec
Yutai Zhou <yutaizh@gmail.com>
2025-03-04 17:19:57 +0000

    bash iscommand

--- bash/plugins.bash
+++ bash/plugins.bash
@@ -1,14 +1,29 @@
+if is_command_verbose fzf; then
+    eval "$(fzf --bash)"
+fi
 
-# mac stuff >> 
-# export BASH_SILENCE_DEPRECATION_WARNING=1
-# eval "$(/opt/homebrew/bin/brew shellenv)"
-# export TERM=screen-256color-bce
-# <<
+if is_command_verbose starship; then
+    eval "$(starship init bash)"
+fi
+
+if is_command_verbose zoxide; then
+    eval "$(zoxide init bash)"
+fi
+
+if is_command_verbose thefuck; then
+    eval "$(thefuck --alias fk)"
+fi
+
+if [ -f $HOME/.fzf.bash ]; then
+    source $HOME/.fzf.bash
+fi
+
+if is_command_verbose uv; then
+    eval "$(uv generate-shell-completion zsh)"
+fi
+
+if is_command_verbose uvx; then
+    eval "$(uvx --generate-shell-completion zsh)"
+fi
 
 
-eval "$(starship init bash)"
-eval "$(fzf --bash)"
-eval "$(zoxide init bash)"
-eval "$(thefuck --alias fk)"
-
-[ -f $HOME/.fzf.bash ] && source $HOME/.fzf.bash
